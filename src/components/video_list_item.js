6b89395e577c8d01d56640ca242b8d47d3e771a0
Mayowa Adegbola <mayowa.adegbola@andela.com>
2017-06-23 00:32:29 +0100

    add video list category

---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+  let video = props.video;
+  const ImageURL = video.snippet.thumbnails.default.url
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src = {ImageURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyAlNZA5h7W2qFv3rHj6U_Qa2eBerX427s4';
 
-const App =  () => {
-  return <div>Hi!</div>;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []}
+    YTSearch({key: API_KEY, term: 'dbanj'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+
+
+  
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos = {this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
+
+9
